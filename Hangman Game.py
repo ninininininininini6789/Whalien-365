df94b1b7286fe9328ba7b24697229391da56a9ac
ninininininininini6789 <kon.hasini@gmail.com>
2026-04-10 15:33:19 +1000

    my hangman game pls

--- Hangman Game.py
+++ Hangman Game.py
@@ -1,20 +1,128 @@
 import random
+import words.txt
+print(r'''_
+      | |
+      | |__   __ _ _ __   __ _ _ __ ___   __ _ _ __  
+      | '_ \ / _` | '_ \ / _` | '_ ` _ \ / _` | '_ \ 
+      | | | | (_| | | | | (_| | | | | | | (_| | | | |
+      |_| |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|
+                          __/ |                      
+                         |___/                   ''')
+stages = [r'''
+    ___________
+     |/      |
+     |      (_)
+     |      \|/
+     |       |
+     |      / \
+     |
+    _|___
+    
+    ''',r'''
+     ___________
+     |/      |
+     |      (_)
+     |      \|/
+     |       |
+     |      / 
+     |
+    _|___''',r'''
+    ___________
+     |/      |
+     |      (_)
+     |      \|/
+     |       |
+     |      
+     |
+    _|___''', r'''
+    ___________
+     |/      |
+     |      (_)
+     |      \|/
+     |       
+     |      
+     |
+    _|___''', r'''
+    ___________
+     |/      |
+     |      (_)
+     |      \|
+     |       
+     |     
+     |
+    _|___''',r'''
+    ___________
+     |/      |
+     |      (_)
+     |       |
+     |       
+     |      
+     |
+    _|___
+          ''',r'''
+    ___________
+     |/      |
+     |      (_)
+     |       
+     |      
+     |
+    _|___
+          ''',r'''
+    ___________
+     |/      |
+     |      
+     |      
+     |       
+     |      
+     |
+    _|___
+          ''']
+# Creating word by computer
 
-def hangman():
-    words = ["Like", "Interlude", "No", "Coffee", "Paldogangsan", "Tomorrow", "Jump", "Danger", "Rain", "Heaven", "Embarassed", "Dope", "Run", "Butterfly", "Begin", "Lie", "Stigma", "Reflection", "Mama", "Awake", "Lost", "DNA", "Dimple", "Paradise", "Anpanman", "Euphoria", "Her", "Singularity", "Tear", "Idol", "Mikrokosmos", "Home", "Dionysus", "Heartbeat", "Lights", "Filter", "On", "Ugh", "Friends", "Moon", "Respect", "Ego", "Telepathy", "Disease", "Stay", "Dynamite", "Butter", "Hooligan", "Aliens", "Fya", "Swim", "Normal", "Please"]
-    word_to_guess = random.choice(words)
-    guessed_letters = []
-    attempts = 12
-    word_display = ["_"] * len(word_to_guess)
+lives = 7
+user_name = input("Enter your username")
 
-    print("Welcome to Hangman!!!")
-    while attempts > 0 and "_" in word_display:
-        print(f"\nWord: {''.join(word_display)}")
-        print(f"Guessed letters: {','.join(guessed_letters)}")
-        print(f"Attempts Left: {attempts}")
-        guess = input("GAMBLE AND GUESS A LETTER!: ").lower()
+Choosen_word = random.choice(words.word_list).lower()
+# Creating placeholder
+length_of_word = len(Choosen_word)
+placeholder = ""
+for placeholder in range(length_of_word):
+    placeholder+="_"
+print("Word to guess:",placeholder," " "Length of word", length_of_word)
 
-        if guess in guessed_letters:
-            print("You already guessed that letter!!! Your being a bit dumb")
-        elif guess in word_to_guess:
-            print("YAY! ")
+# Guessing number and checking
+Game_over = False
+correct_letter = []
+while not Game_over:
+
+    guess = input(f"Guess a letter{user_name}!!: ").lower()
+
+    if guess in correct_letters:
+        print(f"Aw! You've already guessed the letter {guess} {user_name}!.")
+
+    display = ""
+
+    for letter in Choosen_word:
+        if letter == guess:
+            display += letter
+            correct_letter.append(guess)
+        elif letter in correct_letters:
+            display += letter
+        else:
+            display+= "_"
+    print(display)
+
+
+    if guess not in Choosen_word:
+        lives -= 1
+        print(stages[lives])
+        print(f"You guessed {guess}, which is not in the word. You lose a life ૮(˶ㅠ︿ㅠ)ა")
+    if lives == 0:
+        Game_over = True
+        print(f"YOU LOST {user_name}｡°(°.◜ᯅ◝°)°｡ hangman is complete. The word was", Choosen_word+".")
+
+    if "_" not in display:
+        Game_over = True
+        print(f"YOU WIN {user_name}ദ്ദി(˵ •̀ ᴗ - ˵ ) ✧ The word was", Choosen_word+".")
+
+print(stages[lives])
